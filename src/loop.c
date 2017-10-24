9c4465d171e7cc7430f9ef18740df291f79ca123
Alex Margarit <alex@alxm.org>
2017-10-24 16:47:44 -0700

    Rename loop global context var

--- src/loop.c
+++ src/loop.c
@@ -3,36 +3,36 @@
 static struct {
     int x, y;
     SButton up, down, left, right;
-} context;
+} g_context;
 
 void loop_setup(void)
 {
-    context.x = s_screen_getWidth() / 2;
-    context.y = s_screen_getHeight() / 2;
+    g_context.x = s_screen_getWidth() / 2;
+    g_context.y = s_screen_getHeight() / 2;
 
-    context.up = s_buttons[S_BUTTON_UP];
-    context.down = s_buttons[S_BUTTON_DOWN];
-    context.left = s_buttons[S_BUTTON_LEFT];
-    context.right = s_buttons[S_BUTTON_RIGHT];
+    g_context.up = s_buttons[S_BUTTON_UP];
+    g_context.down = s_buttons[S_BUTTON_DOWN];
+    g_context.left = s_buttons[S_BUTTON_LEFT];
+    g_context.right = s_buttons[S_BUTTON_RIGHT];
 }
 
 void loop_tick(void)
 {
-    if(s_button_pressed(context.up)) {
-        context.y--;
-    } else if(s_button_pressed(context.down)) {
-        context.y++;
+    if(s_button_pressed(g_context.up)) {
+        g_context.y--;
+    } else if(s_button_pressed(g_context.down)) {
+        g_context.y++;
     }
 
-    if(s_button_pressed(context.left)) {
-        context.x--;
-    } else if(s_button_pressed(context.right)) {
-        context.x++;
+    if(s_button_pressed(g_context.left)) {
+        g_context.x--;
+    } else if(s_button_pressed(g_context.right)) {
+        g_context.x++;
     }
 }
 
 void loop_draw(void)
 {
     s_draw_fill(false);
-    s_draw_rectangle(context.x - 16, context.y - 16, 32, 32, true);
+    s_draw_rectangle(g_context.x - 16, g_context.y - 16, 32, 32, true);
 }
