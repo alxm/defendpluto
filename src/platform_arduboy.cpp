a594451e9edda41ce968d4959c28c1990ad4d35e
Alex Margarit <alex@alxm.org>
2017-11-05 13:41:39 -0800

    Rename platform.c files to be more descriptive.

--- src/platform_arduboy.cpp
+++ src/platform_arduboy.cpp
@@ -104,16 +104,6 @@ void z_sprite_blit(ZSprite* Sprite, int8_t X, int8_t Y, uint8_t Frame)
     Sprites::drawExternalMask(X, Y, Sprite->image, Sprite->mask, Frame, Frame);
 }
 
-void z_sprite_blitCentered(ZSprite* Sprite, int8_t X, int8_t Y, uint8_t Frame)
-{
-    Sprites::drawExternalMask(X - pgm_read_byte(Sprite->image) / 2,
-                              Y - pgm_read_byte(Sprite->image + 1) / 2,
-                              Sprite->image,
-                              Sprite->mask,
-                              Frame,
-                              Frame);
-}
-
 int8_t z_sprite_getWidth(ZSprite* Sprite)
 {
     return (int8_t)pgm_read_byte(Sprite->image);
