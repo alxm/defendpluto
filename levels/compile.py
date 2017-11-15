a536b8dd8f202a57bed477be694691be6d3ce0c6
Alex Margarit <alex@alxm.org>
2017-11-15 12:29:04 -0800

    Refactored the level script compiler and added lots of error checks.

--- levels/compile.py
+++ levels/compile.py
@@ -19,21 +19,6 @@
 
 import sys
 
-varIds = {}
-
-spriteIds = {
-    'asteroid': 0,
-    'enemy0': 1,
-    'enemy1': 2,
-    'enemy2': 3,
-}
-
-aiIds = {
-    'down': 0,
-    'zigzag': 1,
-    'curve': 2,
-}
-
 class Op:
     numInstructions = 0
 
@@ -46,22 +31,23 @@ class Op:
     def setVarFlag(self, Bytecode, ArgIndex):
         Bytecode[1] |= 1 << ArgIndex
 
-    def checkVar(self, Bytecode, Tokens, Index):
+    def checkVar(self, Compiler, Bytecode, Tokens, Index):
         token = Tokens[Index]
 
-        if token in varIds:
-            if 0 <= Index <= 7:
-                self.setVarFlag(Bytecode, Index)
-                return varIds[token]
-            else:
-                return 0
+        if Compiler.hasVar(token):
+            if Index >= 8:
+                Compiler.error('Only the first 8 arguments may be variables')
+
+            self.setVarFlag(Bytecode, Index)
+
+            return Compiler.getVarId(token)
         else:
             return int(token)
 
-    def compile(self, Tokens):
+    def compile(self, Compiler, Tokens):
         if len(Tokens) != 1 + self.numArgs:
-            print('{} requires {} arguments'.format(Tokens[0], self.numArgs))
-            sys.exit(1)
+            Compiler.error('{} requires {} arguments'
+                            .format(Tokens[0], self.numArgs))
 
         if self.numArgs > 0:
             #
@@ -76,23 +62,23 @@ class Op:
             #
             bytecode = [self.opcode]
 
-        return self.custom_compile(Tokens[1 :], bytecode)
+        return self.custom_compile(Compiler, Tokens[1 :], bytecode)
 
-    def custom_compile(self, Tokens, Bytecode):
+    def custom_compile(self, Compiler, Tokens, Bytecode):
         return Bytecode
 
 class OpSet(Op):
     def __init__(self):
         Op.__init__(self, 2)
 
-    def custom_compile(self, Tokens, Bytecode):
+    def custom_compile(self, Compiler, Tokens, Bytecode):
         #
         # 8b     8b
         # var_id value
         # x      32
         #
-        var_id = varIds[Tokens[0]]
-        value = self.checkVar(Bytecode, Tokens, 1)
+        var_id = Compiler.getVarId(Tokens[0])
+        value = self.checkVar(Compiler, Bytecode, Tokens, 1)
 
         Bytecode.append(var_id)
         Bytecode.append(value)
@@ -103,14 +89,14 @@ class OpInc(Op):
     def __init__(self):
         Op.__init__(self, 2)
 
-    def custom_compile(self, Tokens, Bytecode):
+    def custom_compile(self, Compiler, Tokens, Bytecode):
         #
         # 8b     8b
         # var_id value
         # x      16
         #
-        var_id = varIds[Tokens[0]]
-        value = self.checkVar(Bytecode, Tokens, 1)
+        var_id = Compiler.getVarId(Tokens[0])
+        value = self.checkVar(Compiler, Bytecode, Tokens, 1)
 
         Bytecode.append(var_id)
         Bytecode.append(value)
@@ -121,15 +107,33 @@ class OpLoop(Op):
     def __init__(self):
         Op.__init__(self, 1)
 
-    def custom_compile(self, Tokens, Bytecode):
+    def custom_compile(self, Compiler, Tokens, Bytecode):
+        Compiler.nestedLoopsCount += 1
+
+        if Compiler.nestedLoopsCount > Compiler.nestedLoopsLimit:
+            Compiler.error('Exceeded max nested loops limit of {}'
+                            .format(Compiler.nestedLoopsLimit))
+
         #
         # 8b
         # num_times
         # 10
         #
-        num_times = self.checkVar(Bytecode, Tokens, 0)
+        num_times = self.checkVar(Compiler, Bytecode, Tokens, 0)
 
         Bytecode.append(num_times)
+
+        return Bytecode
+
+class OpEnd(Op):
+    def __init__(self):
+        Op.__init__(self, 0)
+
+    def custom_compile(self, Compiler, Tokens, Bytecode):
+        Compiler.nestedLoopsCount -= 1
+
+        if Compiler.nestedLoopsCount < 0:
+            Compiler.error('Mismatched end instruction')
 
         return Bytecode
 
@@ -137,13 +141,13 @@ class OpWait(Op):
     def __init__(self):
         Op.__init__(self, 1)
 
-    def custom_compile(self, Tokens, Bytecode):
+    def custom_compile(self, Compiler, Tokens, Bytecode):
         #
         # 8b
         # frames
         # 30
         #
-        frames = self.checkVar(Bytecode, Tokens, 0)
+        frames = self.checkVar(Compiler, Bytecode, Tokens, 0)
 
         Bytecode.append(frames)
 
@@ -153,17 +157,17 @@ class OpSpawn(Op):
     def __init__(self):
         Op.__init__(self, 5)
 
-    def custom_compile(self, Tokens, Bytecode):
+    def custom_compile(self, Compiler, Tokens, Bytecode):
         #
         # 8b      8b      4b      4b     8b
         # x_coord y_coord type_id ai_id  ai_args
         # 64      -8      enemy0  zigzag 0
         #
-        x_coord = self.checkVar(Bytecode, Tokens, 0)
-        y_coord = self.checkVar(Bytecode, Tokens, 1)
-        type_id = spriteIds[Tokens[2]]
-        ai_id = aiIds[Tokens[3]]
-        ai_args = self.checkVar(Bytecode, Tokens, 4)
+        x_coord = self.checkVar(Compiler, Bytecode, Tokens, 0)
+        y_coord = self.checkVar(Compiler, Bytecode, Tokens, 1)
+        type_id = Compiler.getEnemyId(Tokens[2])
+        ai_id = Compiler.getAiId(Tokens[3])
+        ai_args = self.checkVar(Compiler, Bytecode, Tokens, 4)
 
         Bytecode.append(x_coord)
         Bytecode.append(y_coord)
@@ -176,7 +180,7 @@ class OpBind(Op):
     def __init__(self):
         Op.__init__(self, 2)
 
-    def custom_compile(self, Tokens, Bytecode):
+    def custom_compile(self, Compiler, Tokens, Bytecode):
         #
         # var_name var_id
         # x        0
@@ -184,65 +188,131 @@ class OpBind(Op):
         var_name = Tokens[0]
         var_id = int(Tokens[1])
 
-        varIds[var_name] = var_id
+        Compiler.setVarId(var_name, var_id)
 
         return None
 
-def main(LevelFile):
-    ops = {
-        'over': Op(),
-        'set': OpSet(),
-        'inc': OpInc(),
-        'loop': OpLoop(),
-        'end': Op(),
-        'wait': OpWait(),
-        'waitclear': Op(),
-        'spawn': OpSpawn(),
+class CompilerTool:
+    def __init__(self, LevelFile):
+        self.levelFile = LevelFile
 
-        # Must always be last, doesn't get compiled
-        'bind': OpBind(),
-    }
+        self.varMaxId = 2
 
-    bytecode = []
+        self.nestedLoopsLimit = 2
+        self.nestedLoopsCount = 0
 
-    with open(LevelFile, 'rU') as f:
-        for line in f:
-            tokens = []
-            rawTokens = line.split()
+        self.lineNumber = 0
+        self.varIds = {}
 
-            for t in rawTokens:
-                if t[0] == ';':
-                    break
+        self.enemyIds = {
+            'asteroid': 0,
+            'enemy0': 1,
+            'enemy1': 2,
+            'enemy2': 3,
+        }
 
-                tokens.append(t)
+        self.aiIds = {
+            'down': 0,
+            'zigzag': 1,
+            'curve': 2,
+        }
 
-            if len(tokens) == 0:
-                continue
+        self.ops = {
+            'over': Op(),
+            'set': OpSet(),
+            'inc': OpInc(),
+            'loop': OpLoop(),
+            'end': OpEnd(),
+            'wait': OpWait(),
+            'waitclear': Op(),
+            'spawn': OpSpawn(),
 
-            instruction = ops[tokens[0]]
-            compiled = instruction.compile(tokens)
+            # Must always be last, doesn't get compiled
+            'bind': OpBind(),
+        }
 
-            if compiled:
-                bytecode.append(compiled)
+    def error(self, Text):
+        print('\033[1;31m[Script Error]\033[0m Line {}: {}'
+                .format(self.lineNumber + 1, Text),
+              file = sys.stderr)
+        sys.exit(1)
 
-    formatted_bytecode = ''
+    def hasVar(self, Name):
+        return Name in self.varIds
 
-    for line in bytecode:
-        formatted_bytecode += '\n    '
+    def getVarId(self, Name):
+        if Name not in self.varIds:
+            self.error('Unknown var {}'.format(Name))
 
-        for byte in line:
-            formatted_bytecode += '0x{:0>2x},'.format((byte + 2**8) % (2**8))
+        return self.varIds[Name]
 
-    contents = """\
+    def setVarId(self, Name, VarId):
+        if Name in self.varIds:
+            self.error('Var {} was already bound'.format(Name))
+        elif VarId >= self.varMaxId:
+            self.error('Max var ID is {}'.format(self.varMaxId))
+
+        self.varIds[Name] = VarId
+
+    def getEnemyId(self, Name):
+        if Name not in self.enemyIds:
+            self.error('Unknown enemy {}'.format(Name))
+
+        return self.enemyIds[Name]
+
+    def getAiId(self, Name):
+        if Name not in self.aiIds:
+            self.error('Unknown AI {}'.format(Name))
+
+        return self.aiIds[Name]
+
+    def getOp(self, Name):
+        if Name not in self.ops:
+            self.error('Unknown op {}'.format(Name))
+
+        return self.ops[Name]
+
+    def run(self):
+        bytecode = []
+
+        with open(self.levelFile, 'rU') as f:
+            for self.lineNumber, line in enumerate(f):
+                tokens = []
+                rawTokens = line.split()
+
+                for t in rawTokens:
+                    if t[0] == ';':
+                        break
+
+                    tokens.append(t)
+
+                if len(tokens) == 0:
+                    continue
+
+                instruction = self.getOp(tokens[0])
+                compiled = instruction.compile(self, tokens)
+
+                if compiled:
+                    bytecode.append(compiled)
+
+        fmt_bytecode = ''
+
+        for line in bytecode:
+            fmt_bytecode += '\n    '
+
+            for byte in line:
+                fmt_bytecode += '0x{:0>2x},'.format((byte + 2**8) % (2**8))
+
+        contents = """\
 // Generated by levels/compile.py
 PROGMEM static const uint8_t z_data_levels[] = {{{}
 }};\
-""".format(formatted_bytecode)
+""".format(fmt_bytecode)
 
-    print(contents)
+        print(contents)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage: {} data.txt'.format(sys.argv[0]))
     else:
-        main(sys.argv[1])
+        CompilerTool(sys.argv[1]).run()
