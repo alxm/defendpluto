87b5384f04162d9fb45247a5b11c33fe3adab106
Alex Margarit <alex@alxm.org>
2018-05-25 13:43:02 -0700

    make clean no longer runs the genclean rule, so generated code files are kept for faster full builds.

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 geany -i DefendPluto/* make/* assets/levels/* assets/gfx/*.py assets/sfx/*.py dev.sh README.md &
 arduino DefendPluto/DefendPluto.ino &
-gimp -n assets/gfx/*.png &
+gimp -n assets/gfx/*.png assets/gfx/*.BMP &
 cd make
